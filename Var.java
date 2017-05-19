190585348ef56264d615af3a11dc73e93f3f2e4d
Leo Klingert <vrasdeler@web.de>
2017-05-19 22:30:09 +0200

    Update Var.java

--- Var.java
+++ Var.java
@@ -3,12 +3,25 @@ import javax.swing.JFrame;
 
 public class Var {
 
-	static JFrame frame;
+		static JFrame frame = new JFrame("Hello World");
 	static int framewidth = 1000, frameheight = 600;
 
 	static JButton btnHelloWorld = new JButton();
+	static JButton btnReset = new JButton();
+	static JButton btnLanguage = new JButton();
 	static JButton btn[] = new JButton[9];
-
+	
+	static String btn0Text = "Deutsch";
+	static String btn1Text = "Englisch";
+	static String btn2Text = "Französisch";
+	static String btn3Text = "Japanisch";
+	static String btn4Text = "Chinesisch";
+	static String btn5Text = "Türkisch";
+	static String btn6Text = "Spanisch";
+	static String btn7Text = "Isländisch";
+	static String btn8Text = "Dänisch";
+	
 	static String text;
+	static String language = "German";
 
 }
