0f1110b0fee57cba08a06aeaaa3835227bc81f72
Leo Klingert <vrasdeler@web.de>
2017-05-19 22:30:42 +0200

    Update GUI.java

--- GUI.java
+++ GUI.java
@@ -8,8 +8,6 @@ public class GUI {
 
 	public GUI() {
 
-		Var.frame = new JFrame(
-				"Hello World - Multilanguage || created by Loewenagent");
 		Var.frame.setSize(1000, 600);
 		Var.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		Var.frame.setLocationRelativeTo(null);
@@ -31,12 +29,23 @@ public class GUI {
 		Var.btnHelloWorld.setFocusPainted(false);
 		Var.btnHelloWorld.setBorder(null);
 		Var.frame.add(Var.btnHelloWorld);
+		
+		Var.btnReset.setVisible(true);
+		Var.btnReset.addActionListener(new ActionHandler());
+		Var.btnReset.setFocusPainted(false);
+		Var.btnReset.setBorder(null);
+		Var.frame.add(Var.btnReset);
+		
+		Var.btnLanguage.setVisible(true);
+		Var.btnLanguage.addActionListener(new ActionHandler());
+		Var.btnLanguage.setFocusPainted(false);
+		Var.btnLanguage.setBorder(null);
+		Var.frame.add(Var.btnLanguage);
 
 		Background background = new Background();
 		background.setBounds(0, 0, Var.framewidth, Var.frameheight);
 		background.setVisible(true);
 		Var.frame.add(background);
-
 		Var.frame.setVisible(true);
 	}
 
