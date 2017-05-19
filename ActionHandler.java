d18e7f16980399e97d804a14ce41216443527b11
Leo Klingert <vrasdeler@web.de>
2017-05-19 22:28:28 +0200

    Update ActionHandler.java

--- ActionHandler.java
+++ ActionHandler.java
@@ -5,6 +5,14 @@ public class ActionHandler implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 
+		if (e.getSource() == Var.btnLanguage) {
+			if (Var.language == "German") {
+				Var.language = "English";
+			} else if (Var.language == "English") {
+				Var.language = "German";
+			}
+		}
+
 		if (e.getSource() == Var.btn[0]) {
 			Var.text = "Hallo Welt!";
 		} else if (e.getSource() == Var.btn[1]) {
@@ -23,10 +31,45 @@ public class ActionHandler implements ActionListener {
 			Var.text = "Hallo heimur!";
 		} else if (e.getSource() == Var.btn[8]) {
 			Var.text = "Hallo wereld!";
+		} else if (e.getSource() == Var.btnReset) {
+			Var.text = "";
 		}
 		
-		Var.btnHelloWorld.setText(Var.text);
+		if (Var.language == "English") {
+			Var.btn0Text = "German";
+			Var.btn1Text = "English";
+			Var.btn2Text = "French";
+			Var.btn3Text = "Japanese";
+			Var.btn4Text = "Chinese";
+			Var.btn5Text = "Turkish";
+			Var.btn6Text = "Spanish";
+			Var.btn7Text = "Icelandic";
+			Var.btn8Text = "Dutch";
+		} else if (Var.language == "German") {
+			Var.btn0Text = "Deutsch";
+			Var.btn1Text = "Englisch";
+			Var.btn2Text = "Französisch";
+			Var.btn3Text = "Japanisch";
+			Var.btn4Text = "Chinesisch";
+			Var.btn5Text = "Türkisch";
+			Var.btn6Text = "Spanisch";
+			Var.btn7Text = "Isländisch";
+			Var.btn8Text = "Dänisch";
+		}
+
+		Var.btn[0].setText(Var.btn0Text);
+		Var.btn[1].setText(Var.btn1Text);
+		Var.btn[2].setText(Var.btn2Text);
+		Var.btn[3].setText(Var.btn3Text);
+		Var.btn[4].setText(Var.btn4Text);
+		Var.btn[5].setText(Var.btn5Text);
+		Var.btn[6].setText(Var.btn6Text);
+		Var.btn[7].setText(Var.btn7Text);
+		Var.btn[8].setText(Var.btn8Text);
 		
+		Var.btnHelloWorld.setText(Var.text);
+		Var.btnLanguage.setText(Var.language);
+
 	}
 
 }
